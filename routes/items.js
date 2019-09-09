9f9427bcd2038b4d7dd55990927228fe33c1e5b2
rahat baksh <rahat392@gmail.com>
2019-09-09 12:36:26 +0600

    Express Backend Ready!!

--- routes/items.js
+++ routes/items.js
@@ -4,15 +4,17 @@ const Item = require('../models/item')
 // Get all items
 router.get('/', async (req, res) => {
     try {
-        const subscribers = await Subscriber.find()
-        res.json(subscribers)
+        const items = await Item.find()
+        res.json(items)
     } catch (err) {
         res.status(500).json({ message: err.message })
     }
   })
 
 // Get one item
-router.get('/:id', (req, res) => {
+router.get('/:id', getItem,(req, res) => {
+
+    res.json(res.item)
 })
 
 // Create one item
@@ -36,13 +38,67 @@ router.post('/', async (req, res) => {
 })
 
 // Update one item
-router.patch('/:id', (req, res) => {
+router.patch('/:id', getItem, async (req, res) => {
+    if (req.body.name != null) {
+        res.item.name = req.body.name
+    }
+
+    if (req.body.owner != null) {
+        res.item.owner = req.body.owner
+    }
+
+    if (req.body.tags != null) {
+        res.item.tags = req.body.tags
+    }
+
+    if (req.body.description != null) {
+        res.item.description = req.body.description
+    }
+
+    if (req.body.price != null) {
+        res.item.price = req.body.price
+    }
+
+    if (req.body.contact_no != null) {
+        res.item.contact_no = req.body.contact_no
+    }
+
+    if (req.body.is_sold != null) {
+        res.item.is_sold = req.body.is_sold
+    }
+
+    try {
+        const updateditem = await res.item.save()
+        res.json(updateditem)
+    } catch {
+        res.status(400).json({ message: err.message })
+    }
 })
 
 // Delete one item
-router.delete('/:id', (req, res) => {
+router.delete('/:id', getItem, async (req, res) => {
     
+    try {
+        await res.item.remove()
+        res.json({ message: 'Deleted This item' })
+      } catch(err) {
+        res.status(500).json({ message: err.message })
+      }
 })
 
+async function getItem(req, res, next) {
+    try {
+        item = await Item.findById(req.params.id)
+
+        if (item == null) {
+            return res.status(404).json({ message: 'Cant find item'})
+        }
+    } catch(err){
+        return res.status(500).json({ message: err.message })
+    }
+
+    res.item = item
+    next()
+}
 
 module.exports = router
